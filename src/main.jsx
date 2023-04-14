0ccca97019dca35f559255b1bf1b90317b2c51cc
Martin Richter <karag2006@gmx.net>
2023-04-14 23:15:58 +0200

    config

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './Pages/App.jsx'
 import {Footer} from './Components'
 import './scss/style.scss'
 
